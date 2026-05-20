70e8daf74c4ddc25250eaa6859fdef071876e322
Cédric Belin <cedx@outlook.com>
2026-05-20 22:43:47 +0200

    Add new tests cases

--- Tests/Format-String.Tests.ps1
+++ Tests/Format-String.Tests.ps1
@@ -12,6 +12,8 @@ Describe "Format-String" {
 		@{ Value = "<span>Hello World!</span>"; AsDocument = $false; Expected = "<span>Hello World!</span>" }
 		@{ Value = "<div><b>FooBar</b></div>"; AsDocument = $true; Expected = "<html>`n`t<head></head>`n`t<body>`n`t`t<div>`n`t`t`t<b>FooBar</b>`n`t`t</div>`n`t</body>`n</html>" }
 		@{ Value = "<div><b>FooBar</b></div>"; AsDocument = $false; Expected = "<div>`n`t<b>FooBar</b>`n</div>" }
+		@{ Value = "<button>Foo</button> <button>Bar</button>"; AsDocument = $true; Expected = "<html>`n`t<head></head>`n`t<body>`n`t`t<button>Foo</button>`n`t`t<button>Bar</button>`n`t</body>`n</html>" }
+		@{ Value = "<button>Foo</button> <button>Bar</button>"; AsDocument = $false; Expected = "<button>Foo</button>`n<button>Bar</button>" }
 	) {
 		Format-HtmlString $value -AsDocument:$asDocument | Should -BeExactly $expected
 	}
