4029ecbae1b773e65c4dde50bd7ec191f6092679
Cédric Belin <cedx@outlook.com>
2026-08-23 03:07:13 +0200

    Add tests for the `New-HtmlDataUri` cmdlet

---
+++ Tests/New-DataUri.Tests.ps1
@@ -0,0 +1,19 @@
+﻿using module ../Html.psd1
+
+<#
+.SYNOPSIS
+	Tests the features of the `New-DataUri` cmdlet.
+#>
+Describe "New-DataUri" {
+	It "should create a data URI from the specified file" {
+		$file = "$PSScriptRoot/../Resources/DataUri.gif"
+		$dataUri = New-HtmlDataUri $file -MediaType image/gif
+		Should-Be ([uri]::new("data:image/gif;base64,R0lGODlhAQABAHAAACwAAAAAAQABAIH///8AAAAAAAAAAAACAkQBADs=")) $dataUri
+	}
+
+	It "should create a data URI from the specified byte stream" {
+		$bytes = Get-Content "$PSScriptRoot/../Resources/DataUri.gif" -AsByteStream
+		$dataUri = New-HtmlDataUri $bytes -MediaType image/gif
+		Should-Be ([uri]::new("data:image/gif;base64,R0lGODlhAQABAHAAACwAAAAAAQABAIH///8AAAAAAAAAAAACAkQBADs=")) $dataUri
+	}
+}
