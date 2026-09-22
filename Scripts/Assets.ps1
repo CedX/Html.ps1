f7448b524f965f79bc48c56c1b5a9658bbf1117d
Cédric Belin <cedx@outlook.com>
2026-09-22 21:47:40 +0200

    Update the build system [skip ci]

--- Scripts/Assets.ps1
+++ Scripts/Assets.ps1
@@ -5,23 +5,23 @@ $cmdletTemplate = Get-Content Resources/CmdletTemplate.cs -Raw
 $cmdletsToExport = [List[string]]::new([string[]] @("New-HtmlCustomElement"))
 
 New-Item Sources/Generated -Force -ItemType Directory | Out-Null
-foreach ($element in (Import-PowerShellDataFile Resources/HtmlElements.psd1).Elements) {
+(Import-PowerShellDataFile Resources/HtmlElements.psd1).Elements | ForEach-Object {
 	$parameters = @{
-		Alias = (Get-Alias $element.Tag -ErrorAction Ignore) ? "$($element.Tag)Tag" : $element.Tag
-		CapitalizedTag = [char]::ToUpperInvariant($element.Tag[0]) + $element.Tag.Substring(1)
-		IsVoid = $element.IsVoid.ToString().ToLowerInvariant()
-		Tag = $element.Tag
+		Alias = (Get-Alias $_.Tag -ErrorAction Ignore) ? "$($_.Tag)Tag" : $_.Tag
+		CapitalizedTag = [char]::ToUpperInvariant($_.Tag[0]) + $_.Tag.Substring(1)
+		IsVoid = $_.IsVoid.ToString().ToLowerInvariant()
+		Tag = $_.Tag
 	}
 
 	$cmdletsToExport.Add("New-Html$($parameters.CapitalizedTag)Element")
 	if (Test-Path "Sources/Elements/$($parameters.CapitalizedTag).cs") { continue }
 
 	$content = $cmdletTemplate
-	foreach ($key in $parameters.Keys) { $content = $content -replace "{$key}", $parameters.$key }
+	$parameters.Keys | ForEach-Object { $content = $content -replace "{$_}", $parameters.$_ }
 	Set-Content "Sources/Generated/$($parameters.CapitalizedTag).g.cs" $content -NoNewline
 }
 
 $cmdletsToExport.Sort()
-$cmdlets = $cmdletsToExport.PSForEach{ "`t`t""$_""" } -join [Environment]::NewLine
+$cmdlets = ($cmdletsToExport | ForEach-Object { "`t`t""$_""" }) -join [Environment]::NewLine
 $content = (Get-Content Html.psd1 -Raw) -replace "CmdletsToExport = @\([^)]+\)", "CmdletsToExport = @(`n$cmdlets`n`t)"
 Set-Content Html.psd1 $content -NoNewline
